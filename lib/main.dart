49c9893eecb0e87ce2a391ddbac8da88bb252504
carlstep <carlstep333@me.com>
2021-05-25 10:37:54 +0200

    created quiz_brain class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -43,14 +45,6 @@ class _QuizPageState extends State<QuizPage> {
       q: 'You can lead a cow down stairs but not up stairs.', a: false);
  */
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
-
   int questionNumber = 0;
 
   @override
@@ -66,7 +60,7 @@ class _QuizPageState extends State<QuizPage> {
             child: Center(
               child: Text(
                 //'This is where the question text will go.',
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -90,7 +84,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   print('user is correct');
                 } else {
@@ -117,7 +111,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == false) {
                   print('user is correct');
                 } else {
--- lib/question.dart
+++ lib/question.dart
@@ -3,9 +3,10 @@ class Question {
   String questionText;
   bool questionAnswer;
 
-  Question({String q, bool a}) {
+  Question(String q, bool a) {
     // this is the Constructor
     questionText = q;
     questionAnswer = a;
   }
 }
+
